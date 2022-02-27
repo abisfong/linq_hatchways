c6d3f19e4b3ef36ac9d14de969d009c584f1d1bc
Abraham Fong <abisfong@ucsc.edu>
2022-02-26 17:30:52 -0800

    Style borders and default font (Raleway)

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
